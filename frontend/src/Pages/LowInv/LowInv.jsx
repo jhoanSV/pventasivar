2703557f0626df6be65161c89b72813c2f4e78f4
TecS1 <tec1sierra@gmail.com>
2024-06-13 17:01:32 -0500

    caja-product improved and LowInv advanced

--- frontend/src/Pages/LowInv/LowInv.jsx
+++ frontend/src/Pages/LowInv/LowInv.jsx
@@ -1,3 +1,4 @@
+import './_LowInv.scss';
 import React, { useEffect, useState } from 'react';
 import { useTheContext } from '../../TheProvider';
 import p_json from '../../products_json_test.json'
@@ -21,26 +22,38 @@ export const LowInv = () => {
     }, []);
 
     return (
-        <section>
+        <section className='LowInv'>
+            <div className='backContainer'>
+                <button className='btnStnd btn1'>
+                    <i className="bi bi-caret-left-square-fill"></i>
+                </button>
+            </div>
+            <div className="Row">
+                <label style={{marginRight: '10px'}}>Buscar:</label>
+                <input type="text" placeholder='Buscar' style={{width: '35%', marginBottom: '10px'}}/>
+            </div>
             <div className="productsContainer">
                 { lista &&
                     lista.slice(0,limit).map((product, index) => {
                         return (
-                            <div className='caja-product' key={index}>
-                                <div className='MBimgContainer'>
-                                    <picture>
-                                        {/* {<source
-                                            type="image/avif"
-                                            //srcSet={imgSrc}
-                                        />} */}
-                                        <img
-                                            src='https://random-image-pepebigotes.vercel.app/api/random-image'
-                                            alt="imgProducto"
-                                            decoding="async"
-                                        />
-                                    </picture>
+                            <div className='caja-product'>
+                                <div className='detailBox' key={index}>
+                                    <div className='MBimgContainer'>
+                                        <picture>
+                                            {/* {<source
+                                                type="image/avif"
+                                                //srcSet={imgSrc}
+                                            />} */}
+                                            <img
+                                                //src='https://random-image-pepebigotes.vercel.app/api/random-image'
+                                                src='https://picsum.photos/200/300'
+                                                alt="imgProducto"
+                                                decoding="async"
+                                            />
+                                        </picture>
+                                    </div>
+                                    <strong>{product.descripcion}</strong>
                                 </div>
-                                <strong>{product.descripcion}</strong>
                             </div>
                         );
                     })
