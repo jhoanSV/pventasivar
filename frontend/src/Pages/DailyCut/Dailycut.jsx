92a521ec820da6684b2df991e4b3bf75b526e036
jhoanSV <jhoanjssv97@outlook.com>
2024-08-08 16:07:41 -0500

    measures list

---
+++ frontend/src/Pages/DailyCut/Dailycut.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import "./_Dailycut.scss";
+import { useNavigate } from 'react-router-dom';
+import { useTheContext } from '../../TheProvider';
+import { TheInput } from '../../Components';
+import { postUpdateInventory } from '../../api';
+
+export const Dailycut=()=>{
+    
+    return (
+        <div>
+            <label>Categorias movidas</label>
+            
+        </div>
+    )
+}
+
+
+
